00bf4f87cc68591565ba719531f9f09f20a27594
Neeru Bhardwaj <39168376+neeru56@users.noreply.github.com>
2018-07-21 20:32:49 +0100

    Update PCA_LDA.R

--- PCA_LDA.R
+++ PCA_LDA.R
@@ -11,11 +11,6 @@ output: html_document
 knitr::opts_chunk$set(echo = TRUE)
 ```
 
-## Consider the digits data contained in the files data0,data1,....data9 (downloaded  from http://cis.jhu.edu/~sachin/digit/digit.html)
-File format:
-"Each file has 1000 training examples. Each training example is of size 28x28 pixels. The pixels are stored as unsigned chars (1 byte) and take values from 0 to 255. The first 28x28 bytes of the file correspond to the first training example, the next 28x28 bytes correspond to the next example and so on."
-We have digits data and associated with every instance of a digit we have an image which is actually translated into a set of 784 variables which are greyscale values of the intensities at different pixel in the image.We have 10000 cases ,on each case we have row which are 784 pixel intensities for the hand drawn digit.We do digit classification using LDA. Two  methods are used here to estimate the error (misclassification rate) of a classifier i.e Resubstitution and Cross-validation. 
-
 Consider the digit data and create four classification data sets each with different X-matrix.
 Load the digits data and create the digit matrix after reading the binary data.
 
@@ -37,7 +32,8 @@ dim(xo)
 
 ## Part(i) Columns are intensities for 300 pixels with greatest variability(overall)
 
-We will construct this matrix by applying standard Deviation on the digits data and filter out the data for the first 300 column which explains maximum variability in the data.
+We will construct this matrix by applying standard Deviation on the digits data and filter out the data for the first 300
+column which explains maximum variability in the data.
 
 
 ```{r} 
@@ -48,9 +44,11 @@ X1 <- xo[,order_sdi[1:300]]
 dim(X1)
 ```
 
-## Part(ii) Columns are obtained by applying PCA(on covariance) to the full set of 784 pixel .Retain the number of components required to explain 90\% of variance.
+## Part(ii) Columns are obtained by applying PCA(on covariance) to the full set of 784 pixel .Retain the number of components 
+required to explain 90% of variance.
 
-We construct this matrix by applying PCA on covariance of the digits data. We find out the percentage of variance explained and build a matrix by taking those components which explains 90 percentage of variance in the data.
+We construct this matrix by applying PCA on covariance of the digits data. We find out the percentage of variance explained and 
+build a matrix by taking those components which explains 90 percentage of variance in the data.
 
 
 
@@ -65,7 +63,10 @@ dim(X2)
 
 ### Part(iii) Columns are discriminant variables produced by LDA analysis of X1
 
-The MASS package contains functions for performing lda() and qda().lda() takes dependent variable, or the variable to be predicted and the variables that will be used to model or predict dependent variable. Predict values are based on a model, we pass the model object to the predict() function. Scores computed as part of the predict() method of objects of class "lda" gives us the discriminant variables. It is returned as component x of the object produced by predict()
+The MASS package contains functions for performing lda() and qda().lda() takes dependent variable, or the variable to be predicted
+and the variables that will be used to model or predict dependent variable. Predict values are based on a model, we pass the model 
+object to the predict() function. Scores computed as part of the predict() method of objects of class "lda" gives us the discriminant
+variables. It is returned as component x of the object produced by predict()
 
 
 ```{r}
@@ -93,8 +94,15 @@ dim(X4)
 
 ### 1. Create 3X3 boxplot of the discriminant vriables found in (iii) and (iv)
 
-LDA creates new variables (LDs) by maximizing variance between groups. These are still linear combinations of original variables, but rather than explain as much variance as possible with each sequential LD, instead they are drawn to maximize the DIFFERENCE between groups along that new variable. Rather than a similarity matrix, LDA uses a comparison matrix of between and within groups sum of squares and cross-products. 
-We have 10 digits in the dataset which will give 9 discriminant variables as per min(j-1).Boxplot gives us sepration between the groups as a function of which disciriminant score.For j=1:9, Z[,j] gives 9 different ways we can classify the data set, using the scores. z1[,1] gives the first way of classifying the data. Likewise,each of these 9 plots gives us 9 ways of classifying digits(0-9).The one where the medians are visually most separated and have less overlap, defines most efficient way of classifying these digits.
+LDA creates new variables (LDs) by maximizing variance between groups. These are still linear combinations of original variables,
+but rather than explain as much variance as possible with each sequential LD, instead they are drawn to maximize the DIFFERENCE 
+between groups along that new variable. Rather than a similarity matrix, LDA uses a comparison matrix of between and within groups
+sum of squares and cross-products. 
+
+We have 10 digits in the dataset which will give 9 discriminant variables as per min(j-1).Boxplot gives us sepration between the 
+groups as a function of which disciriminant score.For j=1:9, Z[,j] gives 9 different ways we can classify the data set, using the scores.
+z1[,1] gives the first way of classifying the data. Likewise,each of these 9 plots gives us 9 ways of classifying digits(0-9).
+The one where the medians are visually most separated and have less overlap, defines most efficient way of classifying these digits.
 
 
 
@@ -127,7 +135,10 @@ Among all the plots, Plot 1 gives us most separated medians thus this discrimina
 ### Plot on same graph the ANOVA statistic evaluating separation between classes for each of the 9 discriminant variables produced by X3 and X4
 
 
-Anova is obtained by plotting sdv values from lda againt numer of discriminant variables.svd- the singular values returned by lda() function , which give the ratio of the between- and within-group standard deviations on the linear discriminant variables. Their squares are the canonical F-statistics.Using this we will plot the separation between classes for each of the 9 discriminant variables produced by matrix X3 and X4.
+Anova is obtained by plotting sdv values from lda againt numer of discriminant variables.svd- the singular values returned by lda() 
+function , which give the ratio of the between- and within-group standard deviations on the linear discriminant variables. 
+Their squares are the canonical F-statistics.Using this we will plot the separation between classes for each of the 9 discriminant
+variables produced by matrix X3 and X4.
 
 ```{r}
   matplot(1:9,cbind(o$svd^2,p$svd^2),pch=20:21,main="ANOVA",
@@ -141,7 +152,9 @@ legend("topright",c("Matrix X3","Matrix X4"),pch=20:21,cex=1)
 
 ### Evaluate re-substitution estimates of misclassification by LDA using each of X1 to X4.
 
-A simple estimate of the error rate can be obtained by trying out the classification procedure on the same data set that has been used to compute the classification functions. This method is commonly referred to as resubstitution. The proportion of misclassifications resulting from resubstitution is called the apparent error rate.
+A simple estimate of the error rate can be obtained by trying out the classification procedure on the same data set that has been
+used to compute the classification functions. This method is commonly referred to as resubstitution. The proportion of 
+misclassifications resulting from resubstitution is called the apparent error rate.
 
 
 ```{r}
@@ -188,7 +201,9 @@ x4_Rmiss=print(table(y,yhat4))
 
 ### Evaluate cross-validation estimates of misclassification by LDA using each of X1 to X4. 
 
-Cross-validation is a n-fold cross-validation, where n is the number of training instances. That is, n classifiers are built for all possible (n-1) element subsets of the training set and then tested on the remaining single instance.This involves no random subsampling and makes maximum use of the data.
+Cross-validation is a n-fold cross-validation, where n is the number of training instances. That is, n classifiers are built 
+for all possible (n-1) element subsets of the training set and then tested on the remaining single instance.This involves no 
+random subsampling and makes maximum use of the data.
 
 ```{r}
 #For X1
@@ -244,11 +259,17 @@ X2 : 0.1276
 X3 : 0.115 
 X4 : 0.12 
 
-The error rate estimated by cross-validation is more than the resubstitution estimate of the error rate. While resubstitution  model may minimize the Mean Squared Error on the training data, it can be optimistic in its predictive error. Whereas in crossvalidation data,as stated above,n classifiers are built for all possible (n-1) element subsets of the training set and then tested on the remaining single instance.By doing this cross-validation loops through all the data and gets classification accuracy for each time, they are then averaged to give a number more representative of overall accuracy.
+The error rate estimated by cross-validation is more than the resubstitution estimate of the error rate. While resubstitution  
+model may minimize the Mean Squared Error on the training data, it can be optimistic in its predictive error. Whereas in 
+crossvalidation data,as stated above,n classifiers are built for all possible (n-1) element subsets of the training set and
+then tested on the remaining single instance.By doing this cross-validation loops through all the data and gets classification
+accuracy for each time, they are then averaged to give a number more representative of overall accuracy.
 
-### For LDA and QDA, compute re-substition and cross-validation estimates of misclassification using X4.Report the results on a single page- 4 sets of labelled 10X10 tables
+### For LDA and QDA, compute re-substition and cross-validation estimates of misclassification using X4.Report the results
+### on a single page- 4 sets of labelled 10X10 tables
 
-LDA assumes that the observations are drawn from a Gaussian distribution with a common covariance matrix across each class. QDA, on the other-hand, provides a non-linear quadratic decision boundary.LDA and QDA algorithm are both based on Bayes theorem.
+LDA assumes that the observations are drawn from a Gaussian distribution with a common covariance matrix across each class. 
+QDA, on the other-hand, provides a non-linear quadratic decision boundary.LDA and QDA algorithm are both based on Bayes theorem.
 
 1.LDA - Resubstition and Cross Validation
 
@@ -298,5 +319,7 @@ Cross-Validation
 X4 : 0.0952 
 
 
-we get some improvement with the QDA model.This suggests that the quadratic form assumed by QDA may capture the true relationship more accurately than the linear forms assumed by LDA.QDA is recommended if the training set is very large, so that the variance of the classifier is not a major concern, or if the assumption of a common covariance matrix is clearly untenable
+we get some improvement with the QDA model.This suggests that the quadratic form assumed by QDA may capture the true 
+relationship more accurately than the linear forms assumed by LDA.QDA is recommended if the training set is very large, 
+so that the variance of the classifier is not a major concern, or if the assumption of a common covariance matrix is clearly untenable
 
